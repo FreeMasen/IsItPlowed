1c41a0eba3cb695349e243bb111bc387ff62612f
FreeMasen <RFM@Herman.local>
2016-03-06 10:27:56 -0600

    testing geolocation api interaction

--- src/JS/map.js
+++ src/JS/map.js
@@ -1,20 +1,20 @@
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 44.65, lng: -93.53},
+        center: {lat: 0.0, lng: 0.0},
         zoom: 12,
-        //disableDefaultUI: true
+        disableDefaultUI: true
     });
 };
 
 function getLocation(address) {
-    var url = "http://ec2-54-183-243-1.us-west-1.compute.amazonaws.com:8080/api/county/scott/lookup.json?"
+    var url = "/api/area/test/lookup.json"
     var search = {
         q: address
     };
     $.getJSON(url, search)
         .done(function( json ){
-            map.center = json["location"]
+            map = initMap($('#map').get(0), json['point'])
         })
         .fail(function (jqxr, textStatus, error ){
             console.log(error);
@@ -36,11 +36,12 @@ function getRouteData(routeId) {
 };
 
 $(function () {
-    $("#navItem").on("click", function() {
+    $("#search").on("click", function() {
 
     })
 })
 
 $(document).ready(function() {
     initMap();
+    getLocation("found-one")
 })
